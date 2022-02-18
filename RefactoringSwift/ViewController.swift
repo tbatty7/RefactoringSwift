0078806f0bd54d7e37a672267c8ba01d0e6b2add
Tim Batty <tbatty7@gmail.com>
2022-02-18 12:03:51 -0500

    Initial Commit

---
+++ RefactoringSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RefactoringSwift
+//
+//  Created by Timothy D Batty on 2/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
