8ee6f6c4fb4add211c37d8c3a51ef7f98e5ac539
Tim Batty <tbatty7@gmail.com>
2022-02-18 14:09:55 -0500

    Created ChangePasswordViewController with buttons and stuff but not wired up

---
+++ RefactoringSwift/ChangePasswordViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ChangePasswordViewController.swift
+//  RefactoringSwift
+//
+//  Created by Timothy D Batty on 2/18/22.
+//
+
+import UIKit
+
+class ChangePasswordViewController: UIViewController {
+
+    @IBOutlet private(set) var cancelBarButton: UIBarButtonItem!
+    @IBOutlet private(set) var oldPasswordTextField: UITextField!
+    @IBOutlet private(set) var newPasswordTextField: UITextField!
+    @IBOutlet private(set) var confirmPasswordTextField: UITextField!
+    @IBOutlet private(set) var submitButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+    }
+
+}
