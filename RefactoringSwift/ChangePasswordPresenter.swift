6c6909ffed26fbdf8f5bb048b2d36e88e2364378
Tim Batty <tbatty7@gmail.com>
2022-03-02 11:38:33 -0500

    Created Presenter with an unowned reference to the ViewCommands

---
+++ RefactoringSwift/ChangePasswordPresenter.swift
@@ -0,0 +1,16 @@
+//
+//  ChangePasswordPresenter.swift
+//  RefactoringSwift
+//
+//  Created by Timothy D Batty on 3/2/22.
+//
+
+import Foundation
+
+class ChangePasswordPresenter {
+    private unowned var view: ChangePasswordViewCommands
+    
+    init(view: ChangePasswordViewCommands) {
+        self.view = view
+    }
+}
